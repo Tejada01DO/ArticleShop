2e1df8cd55db4dc3862c73561693860a45736927
Robert Tejada <robert_tejada@ucne.edu.do>
2023-07-29 08:54:01 -0400

    Agregando la base de datos y inyectando el contexto

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Context>(options => options.UseSqlite(ConStr));
 
 // Add services to the container.
 
